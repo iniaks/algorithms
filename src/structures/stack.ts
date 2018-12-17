97cf5053feb4f7e69535bd2f53ea9db19b13b5f8
qianchen <iniaks@163.com>
2018-12-17 22:51:56 +0800

    stack

---
+++ src/structures/stack.ts
@@ -0,0 +1,30 @@
+class Stack {
+    stack: any[]
+    constrcutor(stack: any[]) {
+        this.stack = stack
+    }
+    // push an element into the peek of stack
+    push(element: any) {
+        this.stack.push(element)
+    }
+    // remove the element of bottom of stack
+    pop() {
+        return this.stack.pop()
+    }
+    // get the peek element of stack
+    peek() {
+        return this.stack[this.stack.length - 1]
+    }
+    isEmpty() {
+        return this.stack.length == 0
+    }
+    size() {
+        return this.stack.length
+    }
+    clear() {
+        this.stack = []
+    }
+    print() {
+        console.log(this.stack.toString())
+    }
+}
